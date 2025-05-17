5368dd5890ee1b4b3c75b64a233dec247cb8d9ed
mactive <mactive@gmail.com>
2025-05-17 10:08:12 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl underline font-thin'>hello word</h1>
+      <h1 className='text-4xl underline font-light'>hello word</h1>
+      <h1 className='text-3xl underline font-normal'>你好，世界</h1>
+      <h1 className='text-2xl underline font-bold'>hello word</h1>
+      <h1 className='text-1xl underline font-black'>hello word</h1>
+    </div>
+  )
+}
+
+export default App
